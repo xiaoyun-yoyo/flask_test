18d963fbf398809f00f3c0ce1537875b5969ce72
xiaoyun <1875251720@qq.com>
2020-12-13 20:23:25 +0800

    up

--- pro_one/hello_demo1.py
+++ pro_one/hello_demo1.py
@@ -24,22 +24,5 @@ def test_url_for():
 
 
 
-@app.route('/index')
-def test_template_test():
-    name = 'Grey Li'
-    movies = [
-        {'title': 'My Neighbor Totoro', 'year': '1988'},
-        {'title': 'Dead Poets Society', 'year': '1989'},
-        {'title': 'A Perfect World', 'year': '1993'},
-        {'title': 'Leon', 'year': '1994'},
-        {'title': 'Mahjong', 'year': '1996'},
-        {'title': 'Swallowtail Butterfly', 'year': '1996'},
-        {'title': 'King of Comedy', 'year': '1999'},
-        {'title': 'Devils on the Doorstep', 'year': '1999'},
-        {'title': 'WALL-E', 'year': '2008'},
-        {'title': 'The Pork of Music', 'year': '2012'},
-    ]
-    return render_template('index.html', name=name, movies=movies)
-
 if __name__ == '__main__':
     app.run(debug=True)
